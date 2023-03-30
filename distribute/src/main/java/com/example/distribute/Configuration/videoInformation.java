e1c3d984ab92e97164f13f398faff6c31c729d25
dlskawo0409 <90461449+dlskawo0409@users.noreply.github.com>
2023-03-30 17:19:26 +0900

    Delete distribute directory

--- distribute/src/main/java/com/example/distribute/Configuration/videoInformation.java
+++
@@ -1,6 +0,0 @@
-package com.example.distribute.Configuration;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record videoInformation(int frameWeight, int frameHeight, int frameCount, double fps, double videoLength) { }
